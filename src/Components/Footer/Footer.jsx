5f9c6043b5db4a4cd064746a48a0e7632aea850e
Alok Rajbongshi <alokraj00061@gmail.com>
2026-07-06 20:10:47 +0600

    logo added in the footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,26 +1,34 @@
 import { Link } from "react-router";
-
+import Logo from "../../assets/sebok_logo.png";
 
 const Footer = () => {
     return (
-        <footer className="bg-neutral-primary-soft">
-            <div className="max-w-7xl mx-auto p-6">
-                <div className="grid grid-cols-1 md:grid-cols-5 gap-8">
+        <footer className="bg-neutral-primary-soft overflow-x-hidden">
+            <div className="max-w-7xl mx-auto px-4 py-6">
+
+                {/* MAIN GRID */}
+                <div className="grid grid-cols-1 md:grid-cols-5 gap-8 w-full">
 
                     {/* Logo + Description */}
-                    <div className="md:col-span-3">
-                        <h2 className="text-2xl font-semibold text-heading mb-3">
-                            সেবক (SEBOK)
-                        </h2>
-                        <p className="text-body text-sm">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    <div className="md:col-span-3 min-w-0">
+                        <div className="flex justify-center md:justify-start">
+                            <img
+                                src={Logo}
+                                alt="logo"
+                                className="w-28 max-w-full h-auto block"
+                            />
+                        </div>
+                        <p className="text-body text-center md:text-start text-sm mt-3 mx-auto md:mx-0 block wrap-break-word">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br />
                             Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                         </p>
                     </div>
-                    <div className="md:col-span-2 grid grid-cols-2 gap-8">
+
+                    {/* Links Section */}
+                    <div className="md:col-span-2 grid grid-cols-2 gap-6 min-w-0">
 
                         {/* Quick Links */}
-                        <div>
+                        <div className="min-w-0">
                             <h2 className="text-lg font-semibold text-heading mb-3">
                                 Quick Links
                             </h2>
@@ -44,20 +52,26 @@ const Footer = () => {
                         </div>
 
                         {/* Contact Info */}
-                        <div>
+                        <div className="min-w-0">
                             <h2 className="text-lg font-semibold text-heading mb-3">
                                 যোগাযোগ তথ্য
                             </h2>
                             <ul className="text-body space-y-2">
-                                <li><Link to="/facebook" className="hover:text-heading">
-                                    Facebook
-                                </Link></li>
-                                <li><Link to="/instagram" className="hover:text-heading">
-                                    Instagram
-                                </Link></li>
-                                <li><Link to="/linkedin" className="hover:text-heading">
-                                    LinkedIn
-                                </Link></li>
+                                <li>
+                                    <Link to="/facebook" className="hover:text-heading">
+                                        Facebook
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to="/instagram" className="hover:text-heading">
+                                        Instagram
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to="/linkedin" className="hover:text-heading">
+                                        LinkedIn
+                                    </Link>
+                                </li>
                             </ul>
                         </div>
 
@@ -70,8 +84,7 @@ const Footer = () => {
                 </div>
 
             </div>
-        </footer>
-
+        </footer >
     );
 };
 
